167f83d33b2bb45d49d52cbd3c002e437e0109fc
Rickson2019 <2528889836@qq.com>
2020-04-27 17:30:13 -0700

    the pieces can now move according to available cells

---
+++ learnJS.js
@@ -0,0 +1,22 @@
+        /*discover the similarities*/
+        
+        // $(div).on("mouseleave", () => {
+        // 	console.log(cell.get_x_coor() + " " + cell.get_y_coor());
+        // 	$(div).css("background-color","green")
+        // })
+
+
+                // $(window).mousedown("click", () => {
+        // 	$(div).css("background-color",cell.color)
+        // 	console.log(cell.get_x_coor() + " " + cell.get_y_coor());
+        // 	if(this.piece != null){
+        // 		this.piece.move_pattern();
+        // 	}
+        // })
+
+
+        
+        // for(let z=0; z<document.getElementsByTagName("img").length;z++)
+      // document.getElementsByTagName("img")[z].onmousedown = function(e){
+        // 	e.preventDefault();
+        // }
